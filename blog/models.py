f7df116f2caf03a88063e083f0fa97604fafb28a
Ivan Weiss <iweiss@gmail.com>
2018-05-18 22:59:39 -0700

    Blog and site polish

--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,22 @@ from django.db import models
 # pub date
 # body
 # image
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title + " : " + str(self.pub_date.strftime('%b %e %Y'))
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
 # Add the Blog app to the settings
 
 # Create a migration
